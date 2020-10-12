0da66bf488a5b6d160af3c12c9a01e6a977c85f0
FabienDaguet <daguet77@gmail.com>
2020-10-12 22:04:27 +0200

    timer code base

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+let remainingTime = document.querySelector(".js-remaining-time");
+let decrease = document.querySelector(".js-btn-decrease");
+let increase = document.querySelector(".js-btn-increase");
+let start = document.querySelector(".js-btn-start");
+let ring = document.querySelector(".js-btn-ring");
+let time = 10;
+let shake = document.querySelector(".alarm");
+remainingTime.textContent = time + " sec.";
+
+
+
+function timeDecrease() {
+    if (time > 0) {
+       time -=1;
+       remainingTime.textContent = time + " sec.";
+    }
+}
+
+function timeIncrease() {
+    if (time >= 0) {
+        time += 1
+        remainingTime.textContent = time + " sec.";
+    }
+}
+
+function stopShaking() {
+    shake.classList.remove("alarm-shaking");
+}
+
+function sonnerie() {
+    let audioObj = new Audio("https://cdn.glitch.com/125a1df2-e0c0-42fc-9a9e-83a539ae87f0%2F219244__zyrytsounds__alarm-clock-short.wav?v=1602505754161");
+    audioObj.play();
+    let shaking = shake.classList.add("alarm-shaking");
+    setInterval(stopShaking, 8500);
+}
+
+function timeStart() {
+      if (time > 0) {
+      setInterval(timeDecrease, 1000);
+      } else {
+        audioObj.play();
+        let shaking = shake.classList.add("alarm-shaking");
+        setInterval(stopShaking, 8500);
+      }
+  }
+
+
+
+
+
+
+
+decrease.addEventListener("click", timeDecrease);
+increase.addEventListener("click", timeIncrease);
+ring.addEventListener("click", sonnerie);
+start.addEventListener("click", timeStart);
